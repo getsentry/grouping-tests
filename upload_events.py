b7d2874768001181443e25062d9229d9811455dd
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-05-06 12:37:24 +0200

    fix filenames usage

--- upload_events.py
+++ upload_events.py
@@ -21,20 +21,21 @@ def upload_events(event_dir, dsn, expire_days):
 
     filenames = glob.glob(f"{event_dir}/**/*json", recursive=True)
 
-    for filename in click.progressbar(filenames):
-        with open(filename) as f:
-            event = json.load(f)
-            event.pop('event_id', None)
-            event.pop('project', None)
-            if expire_days:
-                # Snuba retention is timestamp-based.
-                event['timestamp'] = now - ((expire_days - 90) * 3600 * 24)
-            else:
-                event.pop('timestamp', None)
-            event.pop('threads', None)
-            event.pop('debug_meta', None)
+    with click.progressbar(filenames) as filenames2:
+        for filename in filenames2:
+            with open(filename) as f:
+                event = json.load(f)
+                event.pop('event_id', None)
+                event.pop('project', None)
+                if expire_days:
+                    # Snuba retention is timestamp-based.
+                    event['timestamp'] = now - ((expire_days - 90) * 3600 * 24)
+                else:
+                    event.pop('timestamp', None)
+                event.pop('threads', None)
+                event.pop('debug_meta', None)
 
-            client.transport._send_event(event)
+                client.transport._send_event(event)
 
 
     print(f"Done. Elapsed time is {time.time() - now} secs.")
